03e53728d79cffa7d89c44d398c038be642793a0
Alayan-stk-2 <Alayan-stk-2@users.noreply.github.com>
2026-09-08 22:13:30 +0200

    Fix indentation

--- src/karts/ghost_kart.cpp
+++ src/karts/ghost_kart.cpp
@@ -248,11 +248,11 @@ void GhostKart::update(int ticks)
     }
     else
     {
-    getKartGFX()->setGFXFromReplay(m_all_replay_events[idx].m_nitro_usage,
-                                   m_all_replay_events[idx].m_zipper_usage,
-                                   m_all_replay_events[idx].m_skidding_effect,
-                                   m_all_replay_events[idx].m_red_skidding,
-                                   m_all_replay_events[idx].m_purple_skidding);
+        getKartGFX()->setGFXFromReplay(m_all_replay_events[idx].m_nitro_usage,
+                                       m_all_replay_events[idx].m_zipper_usage,
+                                       m_all_replay_events[idx].m_skidding_effect,
+                                       m_all_replay_events[idx].m_red_skidding,
+                                       m_all_replay_events[idx].m_purple_skidding);
     }
     getKartGFX()->update(dt);
 
